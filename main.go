f43d2a3a36ee0d26eeca0458b9ff4e671b1786bb
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-07-05 13:05:28 +0100

    new commit

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	// fmt.Println("hello world of gofers")
+// 	var nameOne string = "gboyega"
+// 	fmt.Println(nameOne)
+// }
+
 func main() {
-	// fmt.Println("hello world of gofers")
-	var nameOne string = "mario"
+	var nameOne string = "gboyega"
+	var nameTwo string = "segun"
+	var nameThree string = "ariyo"
 	fmt.Println(nameOne)
+	fmt.Println(nameTwo)
+	fmt.Println(nameThree)
 }
