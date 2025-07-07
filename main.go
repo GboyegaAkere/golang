0ae5d571765a220382d9cf114811bdb47fa35883
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-07-07 12:37:58 +0100

    working on variables

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import "fmt"
 
-// func main() {
-// 	// fmt.Println("hello world of gofers")
-// 	var nameOne string = "gboyega"
-// 	fmt.Println(nameOne)
-// }
-
 func main() {
-	var nameOne string = "gboyega"
-	var nameTwo string = "segun"
-	var nameThree string = "ariyo"
-	fmt.Println(nameOne)
-	fmt.Println(nameTwo)
-	fmt.Println(nameThree)
+	// fmt.Println("hello goffers")
+	// strings
+	var nameOne string = "bode"
+	var school = "Futa"
+	var nameThree string
+	fmt.Println(nameOne, school, nameThree)
+
+	//Reassigning the variables
+	nameOne = "Gboyega"
+	nameThree = "Tayo"
+
+	fmt.Println(nameOne, nameThree)
+
+	//another way to declare a variable
+	nameFive := "Oluwasegun"
+	fmt.Println(nameFive)
+
+	//Int
+	var numberOne int = 20
+	var numberTwo = 40
+	numberThree := 60
+
+	fmt.Println(numberOne, numberTwo, numberThree)
+
+	//Floats
+	var scoreOne float64 = 55.10
+	scoreTwo := 99.60
+
+	fmt.Println(scoreOne, scoreTwo)
 }
